0cef7145418e85b7508bd8ac662c0f666050b66f
THiagoPing <tg.oliveira.022@gmail.com>
2026-09-02 21:40:30 -0300

    Add Exercicio2

---
+++ ExerciciosAula3/exercicio2.java
@@ -0,0 +1,13 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int idade = 18;
+        int altura = 165;
+        String status = "Presente";
+
+        System.out.println("Minha idade é: "+idade);
+
+        System.out.println("Minha altura é: "+ altura+"cm");
+        
+        System.out.println("Meu status na aula é: "+status);
+    }
+}
